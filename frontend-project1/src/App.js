b3f088670b5c593b6addb76321ef0d4074ee21dd
iqbalk18 <kamaluddiniqbal15@gmail.com>
2023-08-22 02:21:54 +0700

    add file header and footer

--- frontend-project1/src/App.js
+++ frontend-project1/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import Signup from './screens/Signup';
 import Signin from './screens/Signin';
 
@@ -7,10 +9,12 @@ export default function App() {
   return (
     <div className="App" id="main">
       <Router>
+      <Header />
         <Routes>
           <Route path="/" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
---
+++ frontend-project1/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      {/* Your header content */}
+    </header>
+  );
+}
+
+export default Header;
