7222daa29e350edae4739dc23e34b7d40fef7680
iqbalk18 <kamaluddiniqbal15@gmail.com>
2023-08-24 04:33:05 +0700

    add new navbar layout for dashboard

--- frontend-project1/src/screens/Dashboard.js
+++ frontend-project1/src/screens/Dashboard.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Dashboard() {
   return (
-    <div>
-      <ul className="nav nav-pills nav-fill gap-2 p-1 small bg-primary rounded-5 shadow-sm" id="pillNav2" role="tablist" style={{ '--bs-nav-link-color': 'var(--bs-white)', '--bs-nav-pills-link-active-color': 'var(--bs-primary)', '--bs-nav-pills-link-active-bg': 'var(--bs-white)' }}>
-        <li className="nav-item" role="presentation">
-          <button className="nav-link active rounded-5" id="home-tab2" data-bs-toggle="tab" type="button" role="tab" aria-selected="true">Home</button>
-        </li>
-        <li className="nav-item" role="presentation">
-          <button className="nav-link rounded-5" id="profile-tab2" data-bs-toggle="tab" type="button" role="tab" aria-selected="false">Profile</button>
-        </li>
-        <li className="nav-item" role="presentation">
-          <button className="nav-link rounded-5" id="contact-tab2" data-bs-toggle="tab" type="button" role="tab" aria-selected="false">Contact</button>
-        </li>
-      </ul>
-    </div>
+    <nav class="navbar navbar-expand-lg bg-dark ">
+      <div class="container-fluid">
+        <a class="navbar-brand text-white" href="#">Navbar scroll</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarScroll">
+          <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style={{'--bs-scroll-height': '100px'}}>
+            <li class="nav-item">
+              <a class="nav-link active text-white" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Daily
+              </a>
+              <ul class="dropdown-menu bg-dark">
+                <li><a class="dropdown-item text-white" href="#">Departure</a></li>
+                <li><a class="dropdown-item text-white" href="#">Arrival</a></li>
+                <li><hr class="dropdown-divider" /></li>
+                <li><a class="dropdown-item text-white" href="#">Something else here</a></li>
+              </ul>
+            </li>
+          
+          </ul>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
   );
 }
 
